b74dcb5cc19bfec8cdc51e5c2e33ea424326d8e1
thefathomless <82472494+thefathomless@users.noreply.github.com>
2022-06-26 19:33:23 -0500

    Completed setup script

--- OS/Arch Plasma/setup.sh
+++ OS/Arch Plasma/setup.sh
@@ -1,6 +1,41 @@
-sudo pacman --noconfirm -S xorg xorg-xinit plasma firefox vim htop dolphin
+sudo pacman --noconfirm -S xorg xorg-xinit plasma firefox vim htop dolphin fish
 touch .xinitrc
 cat > .xinitrc << EOF
+#!/bin/sh
 
+userresources=$HOME/Xresources
+usermodmap=$HOME/.Xmodmap
+sysresources=/etc/X11/xinit/Xresources
+sysmodmap=/etc/X11/xinit/.Xmodmap
+if [ -f $sysresources ]; then
+
+
+
+    xrdb -merge $sysresources
+
+fi
+
+if [ -f $sysmodmap ]; then
+    xmodmap $sysmodmap
+fi
+if [-f "$userresources" ]; then
+
+
+    xrdb -merge "$userresources"
+fi
+
+if [ -f "$usermodmap" ]; then
+    xmodmap "$usermodmap"
+fi
+
+if [ -d /etc/X11/xinit/xinitrc.d ] ; then
+ for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
+  [ -x "$f" ] && . "$f"
+ done
+ unsetf
+fi
+
+exec startplasma-x11
 EOF
-reboot now
+
+chsh -s /bin/fish
