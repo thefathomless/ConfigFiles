b1c47f07a5f1515b9ed2692ec0468a09a8d0f1a7
thefathomless <82472494+thefathomless@users.noreply.github.com>
2022-06-27 06:06:01 -0500

    Update setup.sh

--- OS/Arch Plasma/setup.sh
+++ OS/Arch Plasma/setup.sh
@@ -1,29 +1,29 @@
 sudo pacman --noconfirm -S xorg xorg-xinit plasma firefox vim htop dolphin fish
 touch .xinitrc
-cat > .xinitrc << EOF
+echo > .xinitrc << EOF
 #!/bin/sh
 
-userresources=$HOME/.Xresources
-usermodmap=$HOME/.Xmodmap
+userresources=\$HOME/.Xresources
+usermodmap=\$HOME/.Xmodmap
 sysresources=/usr/X11R6/lib/X11/xinit/.Xresources
 sysmodmap=/usr/X11R6/lib/X11/xinit/.Xmodmap
 
 # merge in defaults and keymaps
 
-if [ -f $sysresources ]; then
-    xrdb -merge $sysresources
+if [ -f \$sysresources ]; then
+    xrdb -merge \$sysresources
 fi
 
-if [ -f $sysmodmap ]; then
-    xmodmap $sysmodmap
+if [ -f \$sysmodmap ]; then
+    xmodmap \$sysmodmap
 fi
 
-if [ -f $userresources ]; then
-    xrdb -merge $userresources
+if [ -f \$userresources ]; then
+    xrdb -merge \$userresources
 fi
 
-if [ -f $usermodmap ]; then
-    xmodmap $usermodmap
+if [ -f \$usermodmap ]; then
+    xmodmap \$usermodmap
 fi
 
 exec startplasma-x11
