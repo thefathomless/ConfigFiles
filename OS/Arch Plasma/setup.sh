be6573491bb10c098d2c5cf6d7e7d9950686d85a
thefathomless <82472494+thefathomless@users.noreply.github.com>
2022-06-26 20:15:15 -0500

    Update setup.sh

--- OS/Arch Plasma/setup.sh
+++ OS/Arch Plasma/setup.sh
@@ -3,46 +3,27 @@ touch .xinitrc
 cat > .xinitrc << EOF
 #!/bin/sh
 
-userresources=$HOME/Xresources
+userresources=$HOME/.Xresources
 usermodmap=$HOME/.Xmodmap
-sysresources=/etc/X11/xinit/Xresources
-sysmodmap=/etc/X11/xinit/.Xmodmap
+sysresources=/usr/X11R6/lib/X11/xinit/.Xresources
+sysmodmap=/usr/X11R6/lib/X11/xinit/.Xmodmap
+
+# merge in defaults and keymaps
+
 if [ -f $sysresources ]; then
-
-
-
-
-
-
-
     xrdb -merge $sysresources
-
 fi
 
 if [ -f $sysmodmap ]; then
     xmodmap $sysmodmap
 fi
-if [-f "$userresources" ]; then
 
-
-
-
-
-
-
-    xrdb -merge "$userresources"
-
+if [ -f $userresources ]; then
+    xrdb -merge $userresources
 fi
 
-if [ -f "$usermodmap" ]; then
-    xmodmap "$usermodmap"
-fi
-
-if [ -d /etc/X11/xinit/xinitrc.d ] ; then
- for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
-  [ -x "$f" ] && . "$f"
- done
- unsetf
+if [ -f $usermodmap ]; then
+    xmodmap $usermodmap
 fi
 
 exec startplasma-x11
